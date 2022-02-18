ed7120fe0faed2c2a91375152fd1ec2a8eb238fb
PatBoud <patrice.boudreault@gmail.com>
2022-02-18 09:03:46 -0500

    Retrait de la boucle infinie

--- mcmqtt.py
+++ mcmqtt.py
@@ -10,24 +10,20 @@
 # Prérequis:
 #   - Serveur Minecraft JAVA avec Plugin ServerTap
 #   - Fichier "secrets.py" qui contient les informations confidentielles
+#     (voir README pour détails)
 #   - Librairie paho-mqtt -> https://github.com/eclipse/paho.mqtt.python
+#   - Tâche planifiée (via cron, par exemple) qui appelle ce script à intervalle régulier
 #
 
+print("--- MC-MQTT ---")
+print("Initialisation...")
+
 # Importation des librairies
-from time import sleep
 import paho.mqtt.client as mqtt
 import requests
 import datetime
 
 # Importation des paramètres secrets
-# Le fichier secrets.py doit contenir ceci:
-#
-# mqttServer = ""
-# mqttPort = 1883
-# mqttUser = ""
-# mqttPass = ""
-# apiURL = "http://xxxxxx:4567/v1/"
-#
 import secrets
 
 
@@ -38,27 +34,15 @@ mqttTopicVersion = "minecraft/version"
 mqttTopicMotd = "minecraft/motd"
 mqttTopicUptime = "minecraft/uptime"
 mqttTopicPlugins = "minecraft/plugins"
-delais = 10
 
 
-# Initialisation du client MQTT
-def on_connect(client, userdata, flags, rc):
-  print("Connecté au serveur. Result code: "+str(rc))
+print("OK!")
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.username_pw_set(secrets.mqttUser, password=secrets.mqttPass)
-client.connect(secrets.mqttServer, secrets.mqttPort, 60)
-  
+# Obtention des informations de l'API REST du serveur Minecraft
+# et stockage dans des objets JSON.
 
-# Boucle infinie
-while (True):
-  client.loop()
-
-  # Obtention des informations de l'API REST du serveur Minecraft
-  # et stockage dans des objets JSON.
-  print ("Contact de l'API REST")
-
+try:
+  print ("Contact de l'API REST...")
   # Informations serveur
   response = requests.get(secrets.apiURL + "server")
   infoServer = response.json()
@@ -85,6 +69,18 @@ while (True):
   listeJoueurs = listeJoueurs.replace("§4", "")
   listeJoueurs = listeJoueurs.replace("§r", "")
 
+  print ("OK!")
+except:
+  print("ERREUR: Impossible d'obtenir les informations via l'API REST")
+  exit()
+
+
+try:
+  print("Envoi vers MQTT...")
+  # Initialisation du client MQTT
+  client = mqtt.Client()
+  client.username_pw_set(secrets.mqttUser, password=secrets.mqttPass)
+  client.connect(secrets.mqttServer, secrets.mqttPort, 60)
 
   # Publication des données sur le serveur MQTT
   client.publish(mqttTopicUptime, uptime, qos=0, retain=False)
@@ -93,5 +89,13 @@ while (True):
   client.publish(mqttTopicJoueursListe, listeJoueurs)
   client.publish(mqttTopicJoueursNb, nbJoueurs)
 
-  # Attente selon le délais défini
-  sleep(delais)
+  # Déconnexion
+  client.disconnect()
+
+  print("OK!")
+except:
+  print("ERREUR: Impossible de publier sur MQTT")
+
+
+# Fin du programme
+print ("--- Opération complétée ---")
